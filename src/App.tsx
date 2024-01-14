2cf74903183356444d1cc1385e5589e3efcb3215
Hemsundar Paranthaman <hemsundar.paranthaman@outlook.com>
2024-01-14 18:48:24 +0530

    Hem - Add login page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useRoutes } from "react-router-dom";
 import "./App.css";
 import router from "./features/core/presentation/router";
+import "./../src/assets/css/tailwind.css";
+import "./../src/assets/css/icons.css";
 
 function App() {
   const content = useRoutes(router);
